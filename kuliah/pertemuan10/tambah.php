c784e5a02a8ca5052e04b6f4d9acf3567818ec45
mufidahnm <nurulmufidah102@gmail.com>
2022-04-25 07:41:11 +0700

    pertemuan 10

---
+++ kuliah/pertemuan10/tambah.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tambah Data Mahasiswa</title>
+</head>
+
+<body>
+    <h3>Tambah Data Mahasiswa</h3>
+    <form action="" method="POST">
+        <ul>
+            <li>
+                <label>
+                    Nama :
+                    <input type="text" name="nama">
+                </label>
+            </li>
+            <li>
+                <label>
+                    NRP :
+                    <input type="text" name="nrp">
+                </label>
+            </li>
+        </ul>
+    </form>
+</body>
+
+</html>
